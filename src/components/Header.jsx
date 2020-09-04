aea86522f7f5f3898127050338402c2ddf459490
MrBreakIT <johnpike1022@gmail.com>
2020-09-03 19:36:37 -0500

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+const Header = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello Dojo!</h1>
+                <h2>Things I need to do:</h2>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </header>
+        </div>
+    );
+}
+
+
+
+
+
+export default Header;
